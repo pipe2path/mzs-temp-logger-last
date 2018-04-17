fb6403841cf35e8a5c5bddbed1df4047118bef42
km0585 <km0585@yp.com>
2018-04-17 16:50:39 -0700

    added dropdown for entity

--- js/controller2.js
+++ js/controller2.js
@@ -1,8 +1,18 @@
-var app = angular.module('tempLogger', ['angularSpinner']);
+var app = angular.module('tempLogger', ['angularSpinner', 'ngSanitize', 'ui.select']);
 
-app.controller('DashboardCtrl', ['$scope', '$timeout', '$http', '$q', '$filter', 'DashboardStats',
+app.controller('DashboardCtrl', ['$scope', '$timeout', '$http', '$q', '$filter', 'DashboardStats', 
     function($scope, $timeout, $http, $q, $filter, DashboardStats) {
 
+        var data = [];
+        var url = 'https://mzs-tmp-logger-service.herokuapp.com/entity';
+        $http.get(url).then(function (response) {
+            for(var i=0;i<response.data.length;i++){
+                data.push({id: response.data[i].entityId, name: response.data[i].name});
+            };
+            $scope.entities =  data;
+            $scope.selectedEntity = $scope.entities[0];
+        });
+        
         $scope.tempReadings = {"entityId": "", "readingCelsius": "", "dateTimeStamp": ""};
 
         pollData();
@@ -30,35 +40,49 @@ app.controller('DashboardCtrl', ['$scope', '$timeout', '$http', '$q', '$filter',
         $scope.refreshData = function(){
             pollData();
         }
+
+        //$scope.itemArray = [
+        //    {id: 1, name: 'first'},
+        //    {id: 2, name: 'second'},
+        //    {id: 3, name: 'third'},
+        //    {id: 4, name: 'fourth'},
+        //    {id: 5, name: 'fifth'},
+        //];
+
+        //$scope.selected = { value: $scope.itemArray[0] };
+
+        
+
     }]);
+   
 
-function convertUTCDateToLocalDate(date) {
-    var newDate = new Date(date.getTime()+date.getTimezoneOffset()*60*1000);
+    function convertUTCDateToLocalDate(date) {
+        var newDate = new Date(date.getTime()+date.getTimezoneOffset()*60*1000);
 
-    var offset = date.getTimezoneOffset() / 60;
-    var hours = date.getHours();
+        var offset = date.getTimezoneOffset() / 60;
+        var hours = date.getHours();
 
-    newDate.setHours(hours - offset);
+        newDate.setHours(hours - offset);
 
-    return newDate;   
-}
-
-app.factory('DashboardStats', ['$http', '$timeout', function($http, $timeout) {
-    $http.defaults.headers.post["Content-Type"] = "application/json";
-
-    var data = { response: { }, calls: 0 };
-
-    var poller = function () {
-        var url = 'https://mzs-tmp-logger-service.herokuapp.com/temperature'
-        return $http.get(url).then(function (responseData) {
-            data.calls++;
-            data.response = responseData.data;
-            return data;
-        });
-    };
-
-    return {
-        poll: poller
+        return newDate;   
     }
-}]);
+
+    app.factory('DashboardStats', ['$http', '$timeout', function($http, $timeout) {
+        $http.defaults.headers.post["Content-Type"] = "application/json";
+
+        var data = { response: { }, calls: 0 };
+
+        var poller = function () {
+            var url = 'https://mzs-tmp-logger-service.herokuapp.com/temperature'
+            return $http.get(url).then(function (responseData) {
+                data.calls++;
+                data.response = responseData.data;
+                return data;
+            });
+        };
+
+        return {
+            poll: poller
+        }
+    }]);
 
