9435f4189f654003b724fed8d76f669eeca837ed
kevinmenezes@yahoo.com <kevinmenezes@yahoo.com>
2018-04-17 22:43:35 -0700

    added onSelect directive

--- js/controller2.js
+++ js/controller2.js
@@ -41,18 +41,9 @@ app.controller('DashboardCtrl', ['$scope', '$timeout', '$http', '$q', '$filter',
             pollData();
         }
 
-        //$scope.itemArray = [
-        //    {id: 1, name: 'first'},
-        //    {id: 2, name: 'second'},
-        //    {id: 3, name: 'third'},
-        //    {id: 4, name: 'fourth'},
-        //    {id: 5, name: 'fifth'},
-        //];
-
-        //$scope.selected = { value: $scope.itemArray[0] };
-
-        
-
+        $scope.onSelected = function(selectedItem){
+            console.log("item selected");
+        }
     }]);
    
 
